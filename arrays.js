dd6101607373f8266c5bd33120c51e8e5e681fb6
Moshe Srur <moshiko1988@gmail.com>
2017-11-01 16:58:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,8 +24,8 @@ function accessElementInArray(array, index) {
   return array[index]
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array, element) {
-  array.shift(element)
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
   return array
 }
 
