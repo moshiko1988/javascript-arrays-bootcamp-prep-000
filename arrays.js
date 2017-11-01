7ed26b28d934ba24897f37669a806abb00988164
Moshe Srur <moshiko1988@gmail.com>
2017-11-01 16:50:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,7 @@ function addElementToBeginningOfArray(array, element) {
 return [element, ...array]
 
 }
+
 function destructivelyAddElementToBeginningOfArray(array, element) {
 array.unshift(element)
 return array
@@ -12,6 +13,7 @@ return array
 function addElementToEndOfArray(array, element) {
 return [...array, element]
 }
+
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
